fda5336ee16cc12b059349f180d7c7caded153bb
Arne Güldener <arne-@hotmail.de>
2025-05-16 14:57:01 +0200

    Added Seatalk61. Fixed ID of MOB-Seatalk (31->36)

--- src/seatalk/seatalk_61.rs
+++ src/seatalk/seatalk_61.rs
@@ -2,14 +2,14 @@ use super::seatalk::{ParseError, SeatalkMessage, DATA_BYTES, MAX_SEATALK_LENGTH}
 use core::marker::Sized;
 use core::result::Result;
 
-pub struct Sentence31 {}
+pub struct Sentence61 {}
 
 /*
-36  00  01      Cancel MOB (Man Over Board) condition
+61  03  03 00 00 00  Issued by E-80 multifunction display at initialization
 */
-impl SeatalkMessage for Sentence31 {
-    const ID: u8 = 0x31;
-    const LENGTH: usize = 3;
+impl SeatalkMessage for Sentence61 {
+    const ID: u8 = 0x61;
+    const LENGTH: usize = 6;
 
     fn parse_seatalk_data(
         buffer: [u8; MAX_SEATALK_LENGTH],
@@ -22,19 +22,18 @@ impl SeatalkMessage for Sentence31 {
             return Err(ParseError::WrongID);
         } else if message_length != Self::LENGTH {
             return Err(ParseError::WrongLength);
-        } else if (buffer[1] != 0x00) || (buffer[2] != 0x01) {
+        } else if (buffer[2] != 0x03) || (buffer[3] != 0x00)|| (buffer[4] != 0x00)|| (buffer[5] != 0x00) {
             return Err(ParseError::UnexpectedData);
         }
 
-        Ok(Sentence31 {})
+        Ok(Sentence61 {})
     }
 
     fn generate_seatalk_data(&self) -> [u8; MAX_SEATALK_LENGTH] {
         let mut return_buffer = [0u8; MAX_SEATALK_LENGTH];
         return_buffer[0] = Self::ID;
         return_buffer[1] = (Self::LENGTH - DATA_BYTES) as u8;
-        return_buffer[2] = 0x01;
-
+        return_buffer[2] = 0x03;
         return_buffer
     }
 }
