5d723901f7752716352a6580cd7c7097eb773446
Arne Güldener <arne-@hotmail.de>
2025-05-16 13:54:02 +0200

    Updated ship_data_trais for mileage

--- src/ship_data_traits.rs
+++ src/ship_data_traits.rs
@@ -25,3 +25,13 @@ pub trait WindAngle {
     // Returns the wind angle. Depending on datagram the meaning of it may differ (true or magnetic heading, relative to bow, ...)
     fn get_wind_angle_degree(&self) -> u16;
 }
+
+pub trait CurrentMileage {
+    // Returns current trip mileage
+    fn get_current_mileage(&self) -> u32;
+}
+
+pub trait TotalMileage {
+    // Returns total overall mileage
+    fn get_total_mileage(&self) -> u32;
+}
